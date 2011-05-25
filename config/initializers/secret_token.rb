9c8a398c55fd4c72e2ddce9470e501b2a600f84a
MacBook <macbook@MacBook-de-MacBook-2.local>
2011-05-25 06:21:59 -0300

    adding all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sync::Application.config.secret_token = 'f1a8114395d099adc2bb0cb244bcd3da0ab4c7fea6d5ede4452e7a96007068304f7ac7f7baafc6998f1b35e4ae8777ccb3a6ca40625149602aef91714ecacffc'
